8ad98204afe0a68ca34d1a7695ab58b321ce18df
Gguife <gguife.747@gmail.com>
2023-10-03 17:51:50 -0300

    Padrao Strategy

---
+++ src/dio/test.java
@@ -0,0 +1,49 @@
+package dio;
+
+import dio.Strategy.Comportamento;
+import dio.Strategy.ComportamentoAgressivo;
+import dio.Strategy.ComportamentoDefensivo;
+import dio.Strategy.ComportamentoNormal;
+import dio.Strategy.Robo;
+import dio.singleton.SingletonEager;
+import dio.singleton.SingletonLazy;
+import dio.singleton.SingletonLazyHolder;
+
+public class test {
+
+    public static void main(String[] args){
+
+        //Testes relacionados ao Design Pattern Singleton
+        SingletonLazy lazy = SingletonLazy.getInstancia();
+        System.out.println(lazy);
+        lazy = SingletonLazy.getInstancia();
+        System.out.println(lazy);
+
+        SingletonEager eager = SingletonEager.getInstancia();
+        System.out.println(eager);
+        eager = SingletonEager.getInstancia();
+        System.out.println(eager);
+        
+        SingletonLazyHolder holder = SingletonLazyHolder.getInstancia();
+        System.out.println(holder);
+        holder = SingletonLazyHolder.getInstancia();
+        System.out.println(holder);
+    
+        //Testes relacionados ao Design Pattern Strategy
+        Comportamento defensivo = new ComportamentoDefensivo();
+        Comportamento normal = new ComportamentoNormal();
+        Comportamento agressivo = new ComportamentoAgressivo();
+        
+        Robo robo = new Robo();
+        robo.setComportamento(normal);
+        robo.mover();
+        
+        robo.setComportamento(defensivo);
+        robo.mover();
+        
+        robo.setComportamento(agressivo);
+        robo.mover();
+
+        
+    }
+}
